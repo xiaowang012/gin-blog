0e319bf7d469e168a4d2ef5df28de87fa4e48aed
xiaowang012 <1300202481@qq.com>
2022-05-24 16:26:34 +0800

    routes

--- route/routes.go
+++ route/routes.go
@@ -2,30 +2,36 @@ package route
 
 import (
 	"gin-blog/controller"
-
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func Route(r *gin.Engine) *gin.Engine {
 	//根据是否登录跳转到登录页或者主页
-	//r.GET("/", controller.RegisterGET)
-	//注册页面GET
-	r.GET("/register", controller.RegisterGET)
-	//注册界面POST
-	r.POST("/register", controller.RegisterPOST)
-	//登录页面GET
-	r.GET("/login", controller.LoginGET)
-	//登录页面POST
-	r.POST("/login", controller.LoginPOST)
+	//r.GET("/", controller.Host)
 
-	//修改密码页面GET请求
+	//用户相关接口
+	//注册
+	r.GET("/register", controller.RegisterGET)
+	r.POST("/register", controller.RegisterPOST)
+	//登录
+	r.GET("/login", controller.LoginGET)
+	r.POST("/login", controller.LoginPOST)
+	//退出登录 只能用HandleContext方式重定向所以卸载routes中
+	r.GET("/logout", func(ctx *gin.Context) {
+		session := sessions.Default(ctx)
+		session.Delete("currentUser")
+		session.Save()
+		ctx.Request.URL.Path = "/login"
+		r.HandleContext(ctx)
+	})
+	//修改密码
 	r.GET("/updatePassword", controller.ChangePasswordGET)
-	//修改密码页面POST请求
 	r.POST("/updatePassword", controller.ChangePasswordPOST)
 
-	//用户主页GET
+	//Blog首页
 	r.GET("/index", controller.IndexGET)
-	//用户主页POST
-
+	//搜索文章
+	r.POST("/searchArticles", controller.SearchArticles)
 	return r
 }
