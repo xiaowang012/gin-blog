5f9d7f617e80cc3bc0789196b539d51a79d7f8a3
xiaowang012 <1300202481@qq.com>
2022-06-13 16:48:37 +0800

    删除redis相关代码

--- controller/management.go
+++ controller/management.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"gin-blog/common"
 	"gin-blog/models"
@@ -10,13 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
-	"time"
 )
 
 //UserManagement 后台管理用户管理界面
 func UserManagement(ctx *gin.Context) {
-	//获取redis连接
-	rdb := common.GetRedis()
 	//获取db连接
 	db := common.GetDB()
 	//获取当前登录用户
@@ -24,28 +19,7 @@ func UserManagement(ctx *gin.Context) {
 	currentUserInfo := session.Get("currentUser").(UserInfo)
 	//查询用户表的数据，限制返回10条
 	var users []models.Users
-	//将查询的数据存汝redis中
-	//判断redis中是否存在key management_user10 (第一页的10条user数据)
-	val, err := rdb.Get(context.Background(), "management_user10").Bytes()
-	if err != nil {
-		//fmt.Println("search mysql data --------------------")
-		//查询数据库
-		db.Limit(10).Order("created_at desc").Find(&users)
-		byteData, err := json.Marshal(users)
-		if err != nil {
-			fmt.Println("management_user10转换数据错误: " + err.Error())
-		}
-		err = rdb.Set(context.Background(), "management_user10", byteData, time.Minute*10).Err()
-		if err != nil {
-			fmt.Println("SET management_user10错误!:" + err.Error())
-		}
-	} else {
-		//fmt.Println("get redis data--------------")
-		err = json.Unmarshal(val, &users)
-		if err != nil {
-			fmt.Println("解析users 数据失败!")
-		}
-	}
+	db.Limit(10).Order("created_at desc").Find(&users)
 	//返回数据到HTML
 	ctx.HTML(200, "management/user.html", gin.H{
 		"currentUser": currentUserInfo.UserName,
@@ -101,6 +75,7 @@ func UserManagementDisableUser(ctx *gin.Context) {
 	UserIdInt, err := strconv.Atoi(UserId)
 	if err != nil {
 		fmt.Println("参数错误:" + err.Error())
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
 		return
 	}
 	//根据userId 查询用户
@@ -109,11 +84,13 @@ func UserManagementDisableUser(ctx *gin.Context) {
 	//判断用户是否存在
 	if userInfo.ID == 0 {
 		fmt.Println("用户不存在! 无法禁用此账号!")
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
 		return
 	}
 	//判断账号启用状态,如果已经处于禁用状态，则无需再次禁用
 	if userInfo.Active == false {
 		fmt.Println("用户已经处于禁用状态! 无法再次禁用此账号!")
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
 		return
 	}
 	//将用户的Active 字段改为false
@@ -124,6 +101,35 @@ func UserManagementDisableUser(ctx *gin.Context) {
 
 //UserManagementEnableUser 后台管理用户管理启用账号
 func UserManagementEnableUser(ctx *gin.Context) {
+	//获取db连接
+	db := common.GetDB()
+	//获取用户ID参数
+	UserId := ctx.Query("id")
+	//将id 由string转换为int
+	UserIdInt, err := strconv.Atoi(UserId)
+	if err != nil {
+		fmt.Println("参数错误:" + err.Error())
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
+		return
+	}
+	//根据userId 查询用户
+	var userInfo models.Users
+	db.First(&userInfo, UserIdInt)
+	//判断用户是否存在
+	if userInfo.ID == 0 {
+		fmt.Println("用户不存在! 无法启用此账号!")
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
+		return
+	}
+	//判断账号启用状态,如果已经处于启用状态，则无需再次启用
+	if userInfo.Active == true {
+		fmt.Println("用户已经处于启用状态! 无法再次启用此账号!")
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
+		return
+	}
+	//将用户的Active 字段改为false
+	db.Model(&userInfo).Update("active", true)
+	ctx.Redirect(http.StatusMovedPermanently, "/management")
 
 }
 
@@ -134,6 +140,29 @@ func UserManagementUpdateUser(ctx *gin.Context) {
 
 //UserManagementDeleteUser 后台管理用户管理删除用户
 func UserManagementDeleteUser(ctx *gin.Context) {
+	//获取db连接
+	db := common.GetDB()
+	//获取用户ID参数
+	UserId := ctx.Query("id")
+	//将id 由string转换为int
+	UserIdInt, err := strconv.Atoi(UserId)
+	if err != nil {
+		fmt.Println("参数错误:" + err.Error())
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
+		return
+	}
+	//根据userId 查询用户
+	var userInfo models.Users
+	db.First(&userInfo, UserIdInt)
+	//判断用户是否存在
+	if userInfo.ID == 0 {
+		fmt.Println("用户不存在! 无法删除此账号!")
+		ctx.Redirect(http.StatusMovedPermanently, "/management")
+		return
+	}
+	//删除用户
+	db.Delete(&userInfo)
+	ctx.Redirect(http.StatusMovedPermanently, "/management")
 
 }
 
@@ -145,4 +174,14 @@ func UserManagementAddUser(ctx *gin.Context) {
 //UserManagementImportUsers 后台管理用户管理批量导入用户
 func UserManagementImportUsers(ctx *gin.Context) {
 
+}
+
+//PermissionManagement 后台管理权限管理页面
+func PermissionManagement(ctx *gin.Context) {
+
+}
+
+//PermissionManagementPage 后台管理权限管理页面分页
+func PermissionManagementPage(ctx *gin.Context) {
+	
 }
